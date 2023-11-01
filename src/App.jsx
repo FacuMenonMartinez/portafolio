c07ade329b3cadb38a23f7ef445658a2308d2947
Facundo Menon Martinez <facundomenon.m@gmail.com>
2023-11-01 11:19:13 -0300

    estilo base terminado

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,66 @@ import sobreMi from "./recursos/avatar-2.jpeg";
 
 import Ventana from "./componentes/ventana/Ventana";
 import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
 
+  const [menu, setMenu] = useState('Sobre Mi');
+  const [imgBarra, setImgBarra] =useState(sobreMi);
+
+
+  const handleClick = (seccion,icono) => {
+    setMenu(seccion);
+    setImgBarra(icono)
+  }
+
+  useEffect(() => {
+    if (menu === 'Sobre Mi') {
+      document.getElementById('sobreMi-item').classList.add('icono-item-seleccionado');
+      document.getElementById('tecnologias-item').classList.remove('icono-item-seleccionado');
+      document.getElementById('proyectos-item').classList.remove('icono-item-seleccionado');
+      document.getElementById('cv-item').classList.remove('icono-item-seleccionado');
+
+    } else if (menu === 'Tecnologias') {
+      document.getElementById('tecnologias-item').classList.add('icono-item-seleccionado');
+      document.getElementById('sobreMi-item').classList.remove('icono-item-seleccionado');
+      document.getElementById('proyectos-item').classList.remove('icono-item-seleccionado');
+      document.getElementById('cv-item').classList.remove('icono-item-seleccionado');
+    } else if (menu === 'Proyectos') {
+      document.getElementById('proyectos-item').classList.add('icono-item-seleccionado');
+      document.getElementById('tecnologias-item').classList.remove('icono-item-seleccionado');
+      document.getElementById('sobreMi-item').classList.remove('icono-item-seleccionado');
+      document.getElementById('cv-item').classList.remove('icono-item-seleccionado');
+    } else if (menu === 'Curriculum') {
+      document.getElementById('cv-item').classList.add('icono-item-seleccionado');
+      document.getElementById('tecnologias-item').classList.remove('icono-item-seleccionado');
+      document.getElementById('proyectos-item').classList.remove('icono-item-seleccionado');
+      document.getElementById('sobreMi-item').classList.remove('icono-item-seleccionado');
+    }
+  }, [menu])
 
   return (
     <div className="contenedor-general">
       <section className="iconos-secciones">
-        <img className="iconos-escritorio" src={sobreMi} />
-        <img className="iconos-escritorio" src={proyectos} />
-        <img className="iconos-escritorio" src={tecnologias} />
-        <img className="iconos-escritorio" src={cv} />
+        <div id="sobreMi-item" className="icono-item" onClick={() => { handleClick('Sobre Mi', sobreMi) }}>
+          <img className="iconos-escritorio" src={sobreMi} />
+          <p>Sobre Mí</p>
+        </div>
+        <div id="tecnologias-item" className="icono-item" onClick={() => { handleClick('Tecnologias', tecnologias) }}>
+          <img className="iconos-escritorio" src={tecnologias} />
+          <p>Tecnologías</p>
+        </div>
+        <div id="proyectos-item" className="icono-item" onClick={() => { handleClick('Proyectos', proyectos) }}>
+          <img className="iconos-escritorio" src={proyectos} />
+          <p>Proyectos</p>
+        </div>
+        <div id="cv-item" className="icono-item" onClick={() => { handleClick('Curriculum', cv) }}>
+          <img className="iconos-escritorio" src={cv} />
+          <p>Curriculum</p>
+        </div>
+
       </section>
-      <Ventana />
+      <Ventana seccion={menu} icono={imgBarra} />
     </div>
   )
 }
--- src/componentes/ventana/Ventana.jsx
+++ src/componentes/ventana/Ventana.jsx
@@ -3,24 +3,40 @@ import Tecnologias from '../tecnologias/Tecnologias';
 import Cv from '../cv/Cv';
 import Proyectos from '../proyectos/Proyectos';
 import './ventana.css';
+import { useEffect, useState } from 'react';
 
-function Ventana() {
+function Ventana({seccion, icono}) {
+
+    const [seccionRender, setSeccionRender] = useState('Sobre Mi');
+    // const [iconoBarra, setIconoBarra]=useState()
+
+    useEffect(()=>{
+        setSeccionRender(seccion);
+
+    },[seccion])
+
     return (
         <section className="ventana-renderizado">
 
             <div className='barra-superior'>
-                <img />
-                <h4>Nombre de la app</h4>
-                <div>
+                <img src={icono} />
+                <h4>{seccion}</h4>
+                <div className='contenedor-botonera'>
                     <span class="material-symbols-outlined">minimize</span>
                     <span class="material-symbols-outlined">check_box_outline_blank</span>
                     <span class="material-symbols-outlined">close</span>
                 </div>
             </div>
-            {/* <Sobre_mi/> */}
-            {/* <Tecnologias /> */}
-            {/* <Cv/> */}
-            <Proyectos/>
+            {seccionRender === 'Proyectos'
+            ?<Proyectos/>
+            :seccionRender === 'Curriculum'
+            ?<Cv/>
+            :seccionRender === 'Sobre Mi'
+            ?<Sobre_mi/>
+            :seccionRender === 'Tecnologias'
+            ?<Tecnologias />
+            :<Sobre_mi/>}
+            
         </section>
     )
 }
